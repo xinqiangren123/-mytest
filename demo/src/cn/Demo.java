6f91a00fb4740317cf409c536aaa07ce56231a92
renxinqiang <renxinqiang@itcast.com>
2018-02-28 21:59:03 +0800

    123345修改

--- demo/src/cn/Demo.java
+++ demo/src/cn/Demo.java
@@ -3,6 +3,6 @@ package cn;
 public class Demo {
            public static void main(String[] args) {
 			System.out.println("hello");
-			System.out.println("hello12");
+			System.out.println("hello12345");
 		}
 }
