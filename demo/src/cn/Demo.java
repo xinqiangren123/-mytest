a96f74c8d477c31ca4dc0db3d16b8df483a6c6bb
renxinqiang <renxinqiang@itcast.com>
2018-02-28 21:47:49 +0800

    tx2修改

--- demo/src/cn/Demo.java
+++ demo/src/cn/Demo.java
@@ -3,6 +3,6 @@ package cn;
 public class Demo {
            public static void main(String[] args) {
 			System.out.println("hello");
-			System.out.println("hello1");
+			System.out.println("hello12");
 		}
 }
